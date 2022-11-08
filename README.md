81ba566df18403641e18f89604e7b64c93a5b59d
1sxyzn <soyun9902@ewhain.net>
2022-11-09 01:44:58 +0900

    Dynamic Web Page

---
+++ README.md
@@ -0,0 +1,8 @@
+# WEB with Node.js
+
+## JavaScript
+- var : 변수 선언
+- literal : 데이터를 표현하는 방식
+- template literal : \`content ${variable} content\` 를 사용하여 표현 가능, 줄바꿈도 자동으로 인식
+
+## Node.js
--- main.js
+++ main.js
@@ -5,9 +5,9 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = new URL('http://localhost:3000' + _url).searchParams;
-    console.log(queryData.get('id'));
+    var title = queryData.get('id');
     if(_url == '/'){
-      _url = '/index.html';
+      title = 'Welcome!'
     }
     if(_url == '/favicon.ico'){
         response.writeHead(404);
@@ -15,7 +15,30 @@ var app = http.createServer(function(request,response){
         return;
     }
     response.writeHead(200);
-    response.end(queryData.get('id'));
+
+    var fs = require('fs');
+    fs.readFile(`data/${title}`, 'utf8', function(err, description){
+      var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p style="margin-top:45px;">${description}</p>
+        </body>
+        </html>
+        `;
+      response.end(template);
+    });
  
 });
 app.listen(3000);
